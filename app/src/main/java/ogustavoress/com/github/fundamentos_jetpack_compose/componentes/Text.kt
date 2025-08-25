83c775498d4c4d81beb1092c15d77d3d2a240e1b
ogustavoress <ogustavoress@gmail.com>
2025-08-24 23:25:02 -0300

    Alinhamento do Text e Sombra

--- app/src/main/java/ogustavoress/com/github/fundamentos_jetpack_compose/componentes/Text.kt
+++ app/src/main/java/ogustavoress/com/github/fundamentos_jetpack_compose/componentes/Text.kt
@@ -1,28 +1,61 @@
 package ogustavoress.com.github.fundamentos_jetpack_compose.componentes
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import ogustavoress.com.github.fundamentos_jetpack_compose.R
 
 @Composable
 fun SimpleText(){
-    Text(
-        text = stringResource(id = R.string.hello_turma),
-        color = Color.Blue,
-        fontSize = 30.sp,
-        fontStyle = FontStyle.Italic,
-        fontWeight = FontWeight.Bold
-    )
+    Column {
+        Text(
+            text = stringResource(id = R.string.hello_turma),
+            color = Color.Blue,
+            fontSize = 30.sp,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+    }
 }
 
 @Preview
 @Composable
 fun PreviewSimpleText(){
-    SimpleText()
+//    SimpleText()
+    TextShadow()
+}
+
+@Composable
+fun TextShadow(){
+    val offset = Offset(x = 5f, y = 10f)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+    ) {
+        Text(
+            text = stringResource(id = R.string.hello_turma),
+            textAlign = TextAlign.Center,
+            style = TextStyle(
+                fontSize = 24.sp,
+                shadow = Shadow(color = Color.Blue, offset = offset, blurRadius = 5f)
+            )
+        )
+    }
 }
