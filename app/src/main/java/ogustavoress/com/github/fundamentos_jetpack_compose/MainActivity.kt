655e7e8113f998ff48aa2db1280ad5813040caf9
ogustavoress <ogustavoress@gmail.com>
2025-08-24 22:25:42 -0300

    Layout Card Mensagem

--- app/src/main/java/ogustavoress/com/github/fundamentos_jetpack_compose/MainActivity.kt
+++ app/src/main/java/ogustavoress/com/github/fundamentos_jetpack_compose/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -19,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ogustavoress.com.github.fundamentos_jetpack_compose.ui.theme.FundamentosjetpackcomposeTheme
@@ -83,6 +88,7 @@ fun NotificationBadge(modifier: Modifier = Modifier) {
     }
 }
 
+@Preview(showBackground = true)
 @Composable
 fun PreviewNotificationBadge(){
     FundamentosjetpackcomposeTheme {
@@ -97,6 +103,34 @@ fun TextoCustomizado(texto: String, modifier: Modifier = Modifier) {
         color = Color.White,
         modifier = modifier
     )
+}
 
+@Composable
+fun CardMensagem(modifier: Modifier = Modifier) {
+    Row (modifier = modifier.padding(16.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.avatar),
+            contentDescription = "Foto do contato",
+            modifier = Modifier
+                .size(8.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp)) // -> define o tamanho do espaçador
+
+        Column {
+            Text("Autor da mensagem")
+            Spacer(modifier = Modifier.height(4.dp)) // -> cria um espaço entre dois textos
+            Text("Conteudo da mensagem")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewCardMensagem() {
+    FundamentosjetpackcomposeTheme {
+        CardMensagem()
+    }
 }
 
